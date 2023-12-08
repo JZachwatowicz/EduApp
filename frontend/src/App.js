e46ddd2bfcc76aa69c2a3c4d74b8109da8f38ba5
Oliwia <netirin@gmail.com>
2023-12-08 10:31:39 +0100

    Trochę dostępności :*

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,28 @@ import Profile from './pages/Profile';
 import SignUp from "./pages/SignUp";
 
 import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
+import {
+    BreadcrumbsProvider,
+    Breadcrumbs,
+    BreadcrumbsItem
+} from 'react-breadcrumbs-dynamic';
 
 
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
+
+    <div id="App" className="App">
       <header className="App-header">
           <Router>
-              <Navigation/>
+                <Navigation/>
+                <span className="mx-5">
+                    <Breadcrumbs
+                        separator={<b> > </b>}
+                        finalItem={'b'}
+                    />
+                    {props.children}
+                </span>
               <Routes>
                   <Route path={"/"} element={<Home />}/>
                   <Route path={"/Kursy"} element={<Courses />}/>
--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import {BreadcrumbsItem} from "react-breadcrumbs-dynamic";
 const Courses = (props) => {
     const [kursy, setKursy] = useState([{name:"Kurs#1", disc: "Opis kursu 1" }, {name:"Kurs#2", disc: "Opis kursu 2"}, {name: "Kurs#3",disc:"Opis kursu 3"}, {name:"Kurs#4",disc: "opis kursu 4"},{name:"Kurs#5", disc: "Opis kursu 5" }, {name:"Kurs#6", disc: "Opis kursu 6"}]);
     const [currentDesc , setDesc] = useState(0)
@@ -8,19 +9,20 @@ const Courses = (props) => {
     }
     return(
         <div class="container-fluid px-0">
+            <BreadcrumbsItem to='/Kursy'>Kursy</BreadcrumbsItem>
         <div class="row m-5">
             <div class="m-3 bg-primary rounded-4 shadow-sm col-3 p-3 shadow-sm d-flex flex-column">
-                <h3 class="align-self-center">Dostępne kursy</h3>
+                <h3 class="resize align-self-center">Dostępne kursy</h3>
                 {kursy.map( (kurs,index) => (
-                    <button key={index} className="m-2 my-3 p-2 btn btn-trinary shadow-sm" onClick={() => {setDesc(index)}}>{kurs.name}</button>
+                    <button key={index} className="resize m-2 my-3 p-2 btn btn-trinary shadow-sm" onClick={() => {setDesc(index)}}>{kurs.name}</button>
                 ))}
             </div>
             <div class="col-8 container shadow-sm border border-2 border-primary rounded-4 my-3">
                 <form class="m-3 my-4 d-flex flex-column h-100">
-                    <h4 className="m-2">{kursy[currentDesc].name}</h4>
+                    <h4 className="resize m-2">{kursy[currentDesc].name}</h4>
                     <p className="m-2">{kursy[currentDesc].disc}</p>
 
-                    <Link to={'/Kurs/'+currentDesc} className="m-2 my-3 p-3 btn btn-trinary shadow-sm bottom ">Zapisz się na kurs</Link>
+                    <Link to={'/Kurs/'+currentDesc} className="resize m-2 my-3 p-3 btn btn-trinary shadow-sm bottom ">Zapisz się na kurs</Link>
                 </form>
             </div>
         </div>
